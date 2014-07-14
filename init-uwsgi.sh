65e36585b182e4eb36148dc8e0664cbae33d207d
prajnamort <prajnamort@gmail.com>
2014-07-14 13:10:19 +0800

    add init-unsgi.sh to produce ini and sh file, deleted them from git

---
+++ init-uwsgi.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+########## Settings ##########
+APP_NAME="lambdaoj"
+INI_FILE=$APP_NAME.ini
+SH_FILE="run.sh"
+
+CURRENT_PATH=$(pwd)
+CURRENT_USER=$USER
+CURRENT_GROUP=$(groups | cut -d' ' -f1)
+DEFAULT_PORT="9900"
+
+
+
+########## INI_FILE ##########
+read -p "OJ Web User ($CURRENT_USER) : " OJ_WEB_USER
+read -p "OJ Web Group ($CURRENT_GROUP) : " OJ_WEB_GROUP
+read -p "Uwsgi Listem Port ($DEFAULT_PORT) : " OJ_PORT
+
+{
+    printf "[uwsgi]\n"
+    printf "pythonpath = %s\n" $CURRENT_PATH
+    printf "module = %s\n"     $APP_NAME
+    printf "uid = %s\n"        ${OJ_WEB_USER:-$CURRENT_USER}
+    printf "gid = %s\n"        ${OJ_WEB_GROUP:-$CURRENT_GROUP}
+    printf "socket = :%s\n"    ${OJ_PORT:-$DEFAULT_PORT}
+    printf "callable = app\n"
+} > $INI_FILE
+
+
+
+########## SH_FILE ###########
+{
+    echo "#!/usr/bin/env bash"
+    echo
+    echo "cd " $CURRENT_PATH
+    echo
+    echo "source venv/bin/activate"
+    echo "uwsgi --ini lambdaoj.ini"
+} > $SH_FILE
+
+chmod 0755 $SH_FILE
+
+
+
+############ END #############
+exit 0
--- lambdaoj.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~prajnamort/hack/lambdaOJ/
-
-source venv/bin/activate
-uwsgi --ini lambdaoj.ini
