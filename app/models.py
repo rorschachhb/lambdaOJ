10dd9c88d984d0fce7038e26d1ba6c0e7f6a1693
huangbo10 <rorschachhb@gmail.com>
2014-07-14 18:11:13 +0800

    add db descriptioin

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ ROLE_ADMIN = 1
 STATUS_NORMAL = 0
 STATUS_BLOCKED = 1
 
-ACCEPTED = 0
+PENDING = 0
 TIME_LIMIT_EXCEEDED = 1
 MEM_LIMIT_EXCEEDED = 2
 WRONG_ANSWER = 3
@@ -14,6 +14,7 @@ RUNTIME_ERROR = 4
 COMPILE_ERROR = 5
 PRESENTATION_ERROR = 6
 OUTPUT_LIMIT_EXCEEDED = 7
+ACCEPTED = 8
 
 C = 0
 CPP = 1
@@ -22,6 +23,7 @@ PYTHON = 2
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	nickname = db.Column(db.String(64), index = True, unique = True)
+	sid = db.Column(db.String(10), unique = True)
 	email = db.Column(db.String(120), index = True, unique = True)
 	password = db.Column(db.String(120))
 	role = db.Column(db.SmallInteger, default = ROLE_USER)
@@ -46,14 +48,16 @@ class User(db.Model):
 
 class Problem(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
+	problem_id = db.Column(db.Integer, unique = True)
 	title = db.Column(db.String(100))
-	time = db.Column(db.Integer)
-	memory = db.Column(db.Integer)
-	description = db.Column(db.String(100))
-	input_description = db.Column(db.String(100))
-	output_description = db.Column(db.String(100))
-	sample = db.Column(db.String(100))
-	hint = db.Column(db.String(100))
+	time_limit = db.Column(db.Integer)
+	memory_limit = db.Column(db.Integer)
+	description = db.Column(db.Text)
+	input_description = db.Column(db.Text)
+	output_description = db.Column(db.Text)
+	input_sample = db.Column(db.Text)
+	output_sample = db.Column(db.Text)
+	hint = db.Column(db.Text)
 
 class Submit(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -64,5 +68,5 @@ class Submit(db.Model):
 	memory = db.Column(db.Integer)
 	language = db.Column(db.SmallInteger)
 	submit_time = db.Column(db.DateTime)
-	code = db.Column(db.String(100))
-	error_message = db.Column(db.String(100))
+	code_file = db.Column(db.String(100))
+	error_message = db.Column(db.Text)
