34aa911c36006dfa1091a5a097989a3bfec4a0ea
prajnamort <prajnamort@gmail.com>
2014-07-17 22:03:26 +0800

    change max nickname length from 64 to 20

--- app/models.py
+++ app/models.py
@@ -46,7 +46,7 @@ languages = {
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
-	nickname = db.Column(db.String(64), index = True, unique = True)
+	nickname = db.Column(db.String(20), index = True, unique = True)
 	sid = db.Column(db.String(10), unique = True)
 	email = db.Column(db.String(120), index = True, unique = True)
 	password = db.Column(db.String(120))
