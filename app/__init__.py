b255d9c7ae80f24813dbf9bda91998a76ee05567
huangbo10 <rorschachhb@gmail.com>
2014-07-21 02:14:59 +0800

    add admin, without authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask.ext.admin import Admin
+from flask.ext.admin import Admin, BaseView, expose
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.admin.contrib.fileadmin import FileAdmin
+import os.path as op
 
 app = Flask(__name__, static_url_path='/oj/static')
 app.config.from_object('config')
@@ -12,6 +15,13 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-admin = Admin(app)
-
 import views, models
+
+admin = Admin(app, name='lambdaOJ')
+
+path = op.join(op.dirname(__file__), 'static')
+admin.add_view(FileAdmin(path, '/static/', name='Static Files'))
+
+admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.Problem, db.session))
+admin.add_view(ModelView(models.Submit, db.session))
