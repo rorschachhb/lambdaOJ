42577622da78b4327c6414c3670d228424f0882b
huangbo10 <rorschachhb@gmail.com>
2014-07-27 22:19:28 +0800

    fix db bug

--- app/__init__.py
+++ app/__init__.py
@@ -8,19 +8,18 @@ import os.path as op
 import redis
 import ldap
 import crypt
-from config import CSRF_SECRET_KEY
+from config import CSRF_SECRET_KEY, SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__, static_url_path='/oj/static')
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024
 app.config['CSRF_ENABLED'] = True
 app.config['SECRET_KEY'] = CSRF_SECRET_KEY
 
-parent_dir = op.split(op.dirname(__file__))[0]
-db_dir = op.join(parent_dir, 'db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(db_dir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = op.join(db_dir, 'db_repository')
+# parent_dir = op.split(op.dirname(__file__))[0]
+# db_dir = op.join(parent_dir, 'db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(db_dir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = op.join(db_dir, 'db_repository')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_MIGRATE_REPO'] = SQLALCHEMY_MIGRATE_REPO
 
 db = SQLAlchemy(app)
 
