2ef93de0d9485ffbbab63941954687e17b90b996
huangbo10 <rorschachhb@gmail.com>
2014-07-27 18:31:05 +0800

    fix security bug

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,23 @@ import os.path as op
 import redis
 import ldap
 import crypt
+from config import CSRF_SECRET_KEY, LDAP_BINDDN, LDAP_BINDPW
 
 app = Flask(__name__, static_url_path='/oj/static')
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024
 app.config['CSRF_ENABLED'] = True
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SECRET_KEY'] = CSRF_SECRET_KEY
+
+print "1"
 
 parent_dir = op.split(op.dirname(__file__))[0]
 db_dir = op.join(parent_dir, 'db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + op.join(db_dir, 'app.db')
-app.config['SQLALCHEMY_MIGRATE_REPO'] = op.join(db_dir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(db_dir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = op.join(db_dir, 'db_repository')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_MIGRATE_REPO'] = SQLALCHEMY_MIGRATE_REPO
+
+print "2"
 
 db = SQLAlchemy(app)
 
@@ -25,11 +32,16 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
+print "3"
+
 rds = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
-l = ldap.initialize("ldap://lambda.cool:389")
-l.simple_bind_s('ou=oj, ou=services, dc=lambda, dc=cool', 'aoeirtnsqwertoj')
-print 'ldap connected'
+print "4"
+
+l = ldap.initialize("ldap://106.186.122.64:389")
+l.simple_bind_s(LDAP_BINDDN, LDAP_BINDPW)
+
+print "5"
 
 people_basedn = 'ou=people,dc=lambda,dc=cool'
 groups_basedn = 'ou=groups,dc=lambda,dc=cool'
