8ec9eb930cd9749c8e461be993fe3738e98c471d
huangbo10 <rorschachhb@gmail.com>
2014-08-14 19:41:41 +0800

    change SelectField to RadioField

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField, BooleanField, SelectField, IntegerField, TextAreaField
+from wtforms import TextField, PasswordField, BooleanField, RadioField, IntegerField, TextAreaField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, Optional, NumberRange, ValidationError
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from models import *
@@ -18,7 +18,7 @@ class EditForm(Form):
 
 class SubmitForm(Form):
 	problem_id = IntegerField('problem_id', validators = [InputRequired(), NumberRange(min=1)])
-	language = SelectField('language',
+	language = RadioField('language',
                                choices = [(C, 'C'), (CPP, 'C++')],
                                validators = [InputRequired()], coerce=int)
 	upload_file = FileField('upload_file', validators = [FileRequired()])
